9a341bcd2d3966508ca9d3de11f4b7e203864c02
Elham Jafari <89866910+elijafari@users.noreply.github.com>
2021-11-24 12:26:54 -0800

    final project with TMD API for CICCC college
    First version of final Project

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+    api_key: '67f99b90894878a1a2ea062b16e69d80',
+    api_base_url: 'https://api.themoviedb.org/3/',
+    image_base_url: 'https://image.tmdb.org/t/p/w1280'
+}

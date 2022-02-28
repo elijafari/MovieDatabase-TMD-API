8664cfddd3bd65a99ea5e6aee52ea383312a9de3
Elham Jafari <89866910+eljafari@users.noreply.github.com>
2022-02-28 11:35:47 -0800

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,4 +1,4 @@
-const Api_Key='api_key=67f99b90894878a1a2ea062b16e69d80';
+const Api_Key='api_key='; //put your key here
 const baseUrl= 'https://api.themoviedb.org/3/';
 const Api_Url= baseUrl+'discover/movie?sort_by=popularity.desc&'+Api_Key;
 const baseImage= 'https://image.tmdb.org/t/p/w500'
